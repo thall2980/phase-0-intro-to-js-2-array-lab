f9c5af3c44df9ab48e4d0bab86757267c119810c
Tyler Hall <thall2980@gmail.com>
2022-08-30 21:09:26 -0400

    commit tree caeffa3606e42df009334bd028104305700098af

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newArray = [...cats, name];
+    return newArray;
+}
+
+function prependCat(name) {
+     const newArray = [name, ...cats];
+     return newArray;
+}
+
+function removeLastCat() {
+    const newArray = cats.slice(0, cats.length -1);
+    return newArray;
+}
+
+function removeFirstCat() {
+    const newArray = cats.slice(1);
+    return newArray;
+}
